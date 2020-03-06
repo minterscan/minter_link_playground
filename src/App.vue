65a041de09318da724f3bb3cc8ed01f4cab5b529
whitesunset <chester.work@gmail.com>
2020-03-06 20:16:46 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img class="logo" :src="`${publicPath}/img/logo.svg`">
+    <example />
+  </div>
+</template>
+
+<script>
+import Example from './components/Example.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Example
+  },
+  data () {
+    return {
+      publicPath: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : ''
+    }
+  }
+}
+</script>
